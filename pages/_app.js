3ffcbaaeef13c06f9fe797d48da0ab457e0878ea
sorya-m <107857884+sorya-m@users.noreply.github.com>
2023-06-17 21:21:10 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
